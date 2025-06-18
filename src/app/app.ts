9bf545ad95584719c255cc9a9c905db4d151b590
Sebastian <21041285@itdurango.edu.mx>
2025-06-18 16:35:49 -0600

    final

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'fabel';
+  
+ formData = {
+    nombre: '',
+    email: '',
+    telefono: '',
+    servicio: '',
+    mensaje: ''
+  };
+
+  enviarWhatsapp() {
+    const { nombre, email, telefono, servicio, mensaje } = this.formData;
+    const texto = `Hola, mi nombre es ${nombre}.%0A` +
+                  `mi correo de contacto es: ${email}%0A` +
+                  `y mi Teléfono: ${telefono}%0A` +
+                  `,: ${servicio}%0A` +
+                  `Comentarios: ${mensaje}`;
+
+    const numero = '526181584255'; 
+    const url = `https://wa.me/${numero}?text=${texto}`;
+
+    window.open(url, '_blank');
+  }
+
 }
